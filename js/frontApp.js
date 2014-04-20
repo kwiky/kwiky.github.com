47659a0a4e66cfcea63bfb51aa156c1e45402fe4
Steve Grosbois <steve.grosbois@gmail.com>
2014-04-20 09:44:18 +0200

    Menu

--- js/frontApp.js
+++ js/frontApp.js
@@ -7,6 +7,7 @@ config(function($routeProvider) {
   $routeProvider
   	.when('/about', {templateUrl: 'partials/about.html', controller: 'HomeCtrl'})
   	.when('/skills', {templateUrl: 'partials/skills.html'})
+  	.when('/portfolio', {templateUrl: 'partials/portfolio.html'})
   	.when('/photography', {templateUrl: 'partials/photography.html'})
   	.when('/contact', {templateUrl: 'partials/contact.html'})
 	.otherwise({redirectTo: '/about'});
